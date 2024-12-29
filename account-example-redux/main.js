2b1e669806b7ea0422b7cf91f4bc78501cb42894
vamsiteruvai <teruvaivamsi9@gmail.com>
2024-12-29 16:28:19 +0530

    added

---
+++ account-example-redux/main.js
@@ -0,0 +1,12 @@
+import './index.css'
+import Account from './Account'
+import Form from './Form'
+function App(){
+    return(
+        <>
+        <Form/>
+        <Account/>
+        </>
+    )
+}
+export default App
